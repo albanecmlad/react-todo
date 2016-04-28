d0bcda457285e90f0dfc1d280932f568eeb18e98
albanecmlad <althazari@gmail.com>
2016-04-28 23:21:03 +0300

    todoitem toggling

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,29 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+const classnames = require('classnames');
 
 class TodoItem extends React.Component {
+  state = {
+    completed: false,
+  }
   static propTypes = {
     todo: React.PropTypes.string.isRequired,
     onDestroy: React.PropTypes.func.isRequired,
   }
+  handleChange = () => {
+    this.setState({ completed: !this.state.completed });
+  }
   render() {
     return (
-      <li>
+      <li className={classnames({
+        completed: this.state.completed,
+      })}>
         <div className="view">
           <input
             className="toggle"
             type="checkbox"
+            checked={this.state.completed}
+            onChange={this.handleChange}
           />
           <label>
             {this.props.todo}
